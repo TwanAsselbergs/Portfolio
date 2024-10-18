de31a28b9f5ba07c36791e2524e8f82de3455a64
TwanAsselbergs <twanasselbergs@outlook.com>
2024-10-18 02:27:47 +0200

    Refactor HomeSection component to remove unused imports and update styling

--- frontend/src/common/navbar/Navbar.jsx
+++ frontend/src/common/navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
           className="h-[55px] w-[55px] lg:h-[70px] lg:w-[70px] cursor-pointer rounded-full object-cover mb-4 lg:mb-0 ml-4 lg:ml-24 mt-4 border-[#383b40] border-2"
         />
         <div className="hidden md:block">
-          <div className="opacity-0 lg:p-4 lg:pt-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out pointer-events-none group-hover:pointer-events-auto">
+          <div className="opacity-0 lg:p-4 lg:pt-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out pointer-events-none group-hover:pointer-events-auto">
             <div className="absolute left-[78%] transform -translate-x-1/2 mt-4 bg-white rounded-md">
               <div className="relative">
                 <div className="absolute left-1/2 transform -translate-x-1/2 -top-1.5 w-0 h-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-b-8 border-b-white group-hover:pointer-events-auto group-hover:opacity-100"></div>
@@ -52,35 +52,32 @@ const Navbar = () => {
       </div>
       <ul
         className={`hidden lg:flex text-[#c4cfde] justify-start text-[20px] gap-2 items-center font-bold mr-4 lg:mr-24`}>
-        <li className="cursor-pointer duration-200 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
+        <li className="cursor-pointer duration-500 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
           <a href="#home">Home</a>
         </li>
-        <li className="cursor-pointer duration-200 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
-          <a href="#features">Features</a>
-        </li>
-        <li className="cursor-pointer duration-200 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
+        <li className="cursor-pointer duration-500 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
           <a href="#portfolio">Portfolio</a>
         </li>
-        <li className="cursor-pointer duration-200 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
+        <li className="cursor-pointer duration-500 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
           <a href="#resume">Resume</a>
         </li>
-        <li className="cursor-pointer duration-200 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
+        <li className="cursor-pointer duration-500 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
           <a href="#contact">Contact</a>
         </li>
         <div
           className="relative group"
           onMouseEnter={handleDropdownToggle}
           onMouseLeave={handleDropdownToggle}>
-          <li className="text-[#c4cfde] rounded-md px-4 cursor-pointer duration-700">
+          <li className="text-[#c4cfde] rounded-md px-4 cursor-pointer duration-500">
             <p
-              className={`transition-colors duration-700 ease-in-out ${
+              className={`transition-colors duration-500 ease-in-out ${
                 dropdownOpen ? "text-[#2e3136]" : ""
               }`}>
               CV
             </p>
           </li>
           <div className="absolute left-[94%] transform -translate-x-1/2 p-4 pb-0">
-            <div className="left-1/2 transform -translate-x-1/2 mt-5 bg-white rounded-md opacity-0 pointer-events-none group-hover:pointer-events-auto group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+            <div className="left-1/2 transform -translate-x-1/2 mt-5 bg-white rounded-md opacity-0 pointer-events-none group-hover:pointer-events-auto group-hover:opacity-100 transition-opacity duration-500 ease-in-out">
               <div className="relative group-hover:pointer-events-auto group-hover:opacity-100">
                 <div className="absolute left-1/2 transform -translate-x-1/2 -top-1.5 w-0 h-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-b-8 border-b-white group-hover:pointer-events-auto group-hover:opacity-100"></div>
                 <ul className="py-1">
@@ -111,32 +108,27 @@ const Navbar = () => {
         } flex flex-col text-[13px] gap-6 items-center pt-10 border-l border-[#2e3136]`}>
         <li
           onClick={handleToggle}
-          className="cursor-pointer duration-200 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md mt-8">
+          className="cursor-pointer duration-500 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md mt-8">
           <a href="#home">Home</a>
         </li>
         <li
           onClick={handleToggle}
-          className="cursor-pointer duration-200 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
-          <a href="#features">Features</a>
-        </li>
-        <li
-          onClick={handleToggle}
-          className="cursor-pointer duration-200 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
+          className="cursor-pointer duration-500 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
           <a href="#portfolio">Portfolio</a>
         </li>
         <li
           onClick={handleToggle}
-          className="cursor-pointer duration-200 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
+          className="cursor-pointer duration-500 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
           <a href="#resume">Resume</a>
         </li>
         <li
           onClick={handleToggle}
-          className="cursor-pointer duration-200 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
+          className="cursor-pointer duration-500 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
           <a href="#contact">Contact</a>
         </li>
         <li
           onClick={handleToggle}
-          className="cursor-pointer duration-200 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
+          className="cursor-pointer duration-500 hover:bg-[#2e3136] p-2 pr-4 pl-4 rounded-md">
           <a
             href="cv/Twan Asselbergs CV.pdf"
             download
--- frontend/src/page/PortfolioSection.jsx
+++ frontend/src/page/PortfolioSection.jsx
@@ -1,7 +1,104 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 
 const PortfolioSection = () => {
-  return <div id="portfolio" className="w-full min-h-screen"></div>;
+  const [selectedPort, setSelectedPort] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    let url = `${process.env.REACT_APP_API_ROOT}/posts`;
+    axios.get(url).then((res) => {
+      setPosts(res.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        closeModal();
+      }
+    };
+
+    if (selectedPort) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [selectedPort]);
+
+  const openModal = (port) => {
+    setSelectedPort(port);
+  };
+
+  const closeModal = () => {
+    setSelectedPort(null);
+  };
+
+  return (
+    <div id="portfolio" className="w-[90%] border-b border-black py-20 mx-auto">
+      <h1 className="text-[#c4cfde] text-[34px] md:text-[40px] lg:text-[60px] my-2 text-center font-bold">
+        Portfolio
+      </h1>
+      <div className="w-full my-8 flex gap-6 overflow-x-auto">
+        {Object.keys(posts).length
+          ? posts.map((post) => (
+              <div
+                key={post.id}
+                onClick={() => openModal(post)}
+                className="min-w-[60%] md:min-w-[30%] mb-7 md:mb-0 w-full home-icon-btn rounded-lg flex justify-center items-center flex-col cursor-pointer px-6 md:px-8 py-4 hover:bg-[#2e3136] duration-500">
+                <div className="w-full mb-1 portfolio-img rounded-xl pt-4">
+                  <img
+                    src={
+                      post.featured_image_url ||
+                      "https://via.placeholder.com/500"
+                    }
+                    alt={post.title.rendered}
+                    className="w-full h-40 md:h-96 rounded-lg object-cover"
+                  />
+                </div>
+                <div className="w-full flex px-1 my-3 justify-between">
+                  <p className="text-[#c4cfde] text-[24px] font-bold">
+                    {post.title.rendered}
+                  </p>
+                </div>
+              </div>
+            ))
+          : "Loading...."}
+      </div>
+
+      {selectedPort && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-30 flex items-center justify-center z-[9999]">
+          <div ref={modalRef} className="bg-[#212428] w-5/6 rounded-lg">
+            <div className="w-[90%] h-[90vh] md:h-auto relative md:flex-row flex-col justify-center md:gap-[5%] port-click-overlay-bg md:px-8 py-14 items-center flex rounded-xl">
+              <div className="xl:w-[45%] w[90%] sm:w-[60%] md:w-[50] mb-10 md:mb-0 lg:w-[60%] rounded-xl overflow-hidden">
+                <img
+                  src={
+                    selectedPort.featured_image_url ||
+                    "https://via.placeholder.com/500"
+                  }
+                  alt={selectedPort.title.rendered}
+                  className="w-full h-full"
+                />
+              </div>
+              <div className="xl:w-[45%] w-[90%] sm:w-[70%] md:w-[50%] lg:w-[40%]">
+                <h5 className="text-[#c4cfde] text-[14px] mb-3 xl:mb-0 font-semibold">
+                  {selectedPort.title.rendered}
+                </h5>
+                <h1 className="xl:text-[30px] mb-3 lg:mb-5 xl:mb-0 xl:leading-[4.2rem] text[#c4cfde] font-bold">
+                  {selectedPort.excerpt.rendered}
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default PortfolioSection;
